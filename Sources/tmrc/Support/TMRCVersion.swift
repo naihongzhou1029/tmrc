d075b5d255c6928ffc261a9a9de1f6e40b5fe726
naihongzhou1029 <naihongzhou1029@gmail.com>
2026-03-12 23:52:56 +0800

    chore: bump version to v1.1.0

--- Sources/tmrc/Support/TMRCVersion.swift
+++ Sources/tmrc/Support/TMRCVersion.swift
@@ -3,6 +3,6 @@ import Foundation
 public enum TMRCVersion {
     /// Version string for CLI and logs (semver or git describe).
     public static var current: String {
-        "0.1.0"
+        "1.1.0"
     }
 }
